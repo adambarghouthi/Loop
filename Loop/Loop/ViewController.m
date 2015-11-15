e9e2fe8869e01d31c16107136a7d352f2124b249
Basadev <basadev4@gmail.com>
2015-11-14 20:16:39 -0500

    textView working

--- Loop/Loop/ViewController.m
+++ Loop/Loop/ViewController.m
@@ -22,9 +22,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-//    CGFloat width = [UIScreen mainScreen].bounds.size.width;
-//    CGFloat height = [UIScreen mainScreen].bounds.size.height;
-//    
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    CGFloat height = [UIScreen mainScreen].bounds.size.height;
+//
     self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) style:UITableViewStyleGrouped];
     self.tableView.contentInset = UIEdgeInsetsMake(-27.5f, 0.0f, 0.0f, 0.0f);
     self.tableView.delegate = self;
@@ -34,7 +34,7 @@
     [self.view addSubview:self.tableView];
     
     [self.tableView registerClass:[TextCell class] forCellReuseIdentifier:@"textCell"];
-    _msgTextView = [[textView alloc] initWithFrame:self.view.frame];
+    _msgTextView = [[textView alloc] initWithFrame:CGRectMake(0, height-50, width, height/16)];
     [self.view addSubview:_msgTextView];
 }
 
--- Loop/Loop/textView.m
+++ Loop/Loop/textView.m
@@ -8,67 +8,103 @@
 
 #import "textView.h"
 
+static NSInteger MAX_HEIGHT = 105;
+static NSInteger MIN_HEIGHT = 45;
 @implementation textView
 CGFloat _currentKeyboardHeight = 0.0f;
+CGRect previousRect;
+
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         
-        CGFloat width = [UIScreen mainScreen].bounds.size.width;
-        CGFloat height = [UIScreen mainScreen].bounds.size.height;
-        
-        self.tV = [[UITextView alloc] initWithFrame:CGRectMake(15, height-60, 3*width/4, height/16)];
+
+        UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(0,0,frame.size.width , 0.5)];
+        [separatorView setBackgroundColor:[UIColor grayColor]];
+        self.tV = [[UITextView alloc] initWithFrame:CGRectMake(10,separatorView.frame.size.height, 3*frame.size.width/4 , frame.size.height-5)];
+//                   CGRectMake(15, height-60, 3*width/4, height/16)];
         self.tV.font = [UIFont fontWithName:@"AppleSDGothicNeo-Regular" size:15.0f];
         
-        self.sendBtn = [[UIButton alloc] initWithFrame:CGRectMake(self.tV.frame.size.width + 15, height-70, width - self.tV.frame.size.width -30, height/11)];
+        self.sendBtn = [[UIButton alloc] initWithFrame:CGRectMake(self.tV.frame.size.width + 15, 0, frame.size.width - self.tV.frame.size.width -30, frame.size.height)];
         [self.sendBtn setTitle:@"send" forState:UIControlStateNormal];
         [self.sendBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-        
+//
+        [self addSubview:separatorView];
         [self addSubview:self.tV];
         [self addSubview:self.sendBtn];
+        
         self.tV.delegate = self;
-//        [self setBackgroundColor:[UIColor redColor]];
+        [self setBackgroundColor:[UIColor whiteColor]];
         
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
         
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
 
-        
+        previousRect = CGRectZero;
     }
     
     return self;
 }
 
 #pragma textView Delegate
+
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText: (NSString *)text
 {
-    //Detect newline
-    BOOL newLine = [text isEqualToString:@"\n"];
-    if(newLine)
+    
+    if([text isEqualToString:@"\n"]) // new line
     {
-        [self updateTextFieldFrame];
+        [self updateTextFieldFrame:YES];
+    }else {
+        
+        if([text isEqualToString:@""]) // backspace
+        {
+            textView.selectedRange = range;
+            NSString *selectedText = [textView textInRange:[textView selectedTextRange]];
+            
+            if ([selectedText isEqualToString:@"\n"]) { // line change
+                [self updateTextFieldFrame:NO];
+            }
+        }
     }
+
     return YES;
 }
 
--(void) updateTextFieldFrame{
+-(void) updateTextFieldFrame:(BOOL) enlargeHeight{
     
-    CGRect newFrame = [self.tV frame];
-    newFrame.size.height = self.tV.frame.size.height + 9;
-    newFrame.origin.y = self.tV.frame.origin.y - 9;
+    CGRect newTextViewFrame = [self.tV frame];
+    CGRect newFrame = [self frame];
+    
+    if (enlargeHeight && self.tV.frame.size.height < MAX_HEIGHT){
+        
+        //TextView
+        newTextViewFrame.size.height = self.tV.frame.size.height + 9;
+        
+        //TextView Container
+        newFrame.size.height = self.frame.size.height + 9;
+        newFrame.origin.y = self.frame.origin.y - 9;
+
+    }else if (!enlargeHeight && self.tV.frame.size.height > MIN_HEIGHT){
+        
+        newTextViewFrame.size.height = self.tV.frame.size.height - 9;
+
+        newFrame.size.height = self.frame.size.height - 9;
+        newFrame.origin.y = self.frame.origin.y + 9;
+
+    }
     
     [UIView animateWithDuration:0.25 animations:^
      {
-         [self.tV setFrame:newFrame];
+         [self.tV setFrame:newTextViewFrame];
+         [self setFrame:newFrame];
      }];
+
 }
 
 - (void) textViewDidBeginEditing:(UITextView*)textView {
     
 }
-- (void) textViewDidChange:(UITextView*)textView {
-    
-}
+
 
 #pragma keyboard events
 - (void)keyboardWillShow:(NSNotification*)notification {
